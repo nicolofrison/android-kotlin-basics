dd562b679028133df0ad627d505f3f9f3e45100a
Nicolo <nicolo.frison97@gmail.com>
2023-07-25 13:44:39 -0400

    Added some instrumental tests to TIpTime app

--- README.md
+++ README.md
@@ -66,8 +66,14 @@ Simple app projects used to learn android development with Kotlin. For each app 
 -   Adaptive icons were introduced to the Android platform in API 26. They are made up of a foreground and background layer that follow specific requirements, so that your app icon looks high-quality on a range of devices with different OEM masks.
 -   Use Image Asset Studio in Android Studio to create legacy and adaptive icons for your app.
 
+### Apply Material UI
 -   Use Material Design Components where possible to adhere to Material Design guidelines and allow for more customization.
 -   Add icons to give users visual cues about how parts of your app will function.
 -   Use  `ConstraintLayout`  to position elements in your layout.
 -   Test your app for edges cases (e.g. rotating your app in landscape mode) and make improvements where applicable.
 -   Reformat your code and clean up your code to make it as concise as possible.
+
+### Instrumental testing
+-   Android Studio generates the necessary test classes when the project is created. However, if you encounter a project that doesn't have them, you can create them manually.
+-   Test rules run before every test in a test class.
+-   Espresso is a fundamental component of instrumentation tests. It enables interaction with UI components using code.
--- TipTime/app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ TipTime/app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,12 +1,23 @@
 package com.example.tiptime
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import java.text.DecimalFormat
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +26,69 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.tiptime", appContext.packageName)
+    }
+
+    @Test
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.edit_text_service_cost))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.button_calculate))
+            .perform(click())
+
+        onView(withId(R.id.text_tip_result))
+            .check(matches(withText(containsString("$10.00"))))
+    }
+
+    @Test
+    fun calculate_18_percent_tip_with_round_up() {
+        val serviceCost = 175.85
+
+        onView(withId(R.id.edit_text_service_cost))
+            .perform(typeText(serviceCost.toString()))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.radio_button_second_option))
+            .perform(click())
+
+        onView(withId(R.id.button_calculate))
+            .perform(click())
+
+        val expectedTipResult = Math.ceil(serviceCost * 0.18)
+
+        onView(withId(R.id.text_tip_result))
+            .check(matches(withText(containsString(expectedTipResult.toString()))))
+    }
+
+    @Test
+    fun calculate_18_percent_tip_without_round_up() {
+        val serviceCost = 175.85
+
+        onView(withId(R.id.edit_text_service_cost))
+            .perform(typeText(serviceCost.toString()))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.radio_button_second_option))
+            .perform(click())
+
+        onView(withId(R.id.switch_round_up))
+            .perform(click())
+
+        onView(withId(R.id.button_calculate))
+            .perform(click())
+        val decimalFormat = DecimalFormat("#,###.##")
+        val expectedTipResult = serviceCost * 0.18
+
+        onView(withId(R.id.text_tip_result))
+            .check(matches(withText(containsString(decimalFormat.format(expectedTipResult)))))
     }
 }
